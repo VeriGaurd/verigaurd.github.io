9fe22888c71208e26272661e1b180c1e82f0669f
VeriGaurd <johnbaldwinwon@gmail.com>
2025-04-20 14:44:54 -0700

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,12 +1,13 @@
 
 async function notBannedFromServer() {
   const searchParams = new URLSearchParams(window.location.search);
-  user = searchParams.get('user');
+  discord = searchParams.get('discord');
+  user = searchParams.get('key');
 
     try {
         response = await fetch('https://api.ipify.org?format=json');
         data = await response.json();
-        link = 'https://35.185.88.35:5000/robot?user='+user+'&ip='+data.ip
+        link = 'https://0351ef4b-a5fb-477a-9df3-dd35782c9695-00-30aukvo4epq2k.janeway.replit.dev/robot?discord='+discord"'&user='+user+'&ip='+data.ip
         response = await fetch(link);
     } catch (error) {
         
